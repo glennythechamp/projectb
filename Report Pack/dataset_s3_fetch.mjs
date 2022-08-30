4cd80b5105791cb83428968db31ffd05fc1b86ac
VAKISHNA THAYALAN <19279795@students.ltu.edu.au>
2022-08-30 17:17:43 +1000

    Test Commit2

--- Report Pack/dataset_s3_fetch.mjs
+++ Report Pack/dataset_s3_fetch.mjs
@@ -4,8 +4,8 @@ import { createReadStream, createWriteStream } from "fs";
 
 const file = "index.xlsx"; // Path to and name of object. For example '../myFiles/index.js'.
 const fileStream = createReadStream(file);
-
-
+const downloadPath = 'fds.csv';
+const outputStream = createWriteStream(downloadPath);
 
 const s3Client = new S3Client({
     endpoint: "https://sgp1.digitaloceanspaces.com",
@@ -43,36 +43,14 @@ const uploadObject = async () => {
   
 const getFinancialDataset = async () => {
   try {
-    // Create a helper function to convert a ReadableStream to a string.
-    const streamToString = (stream) =>
-      new Promise((resolve, reject) => {
-        const chunks = [];
-        stream.on("data", (chunk) => chunks.push(chunk));
-        stream.on("error", reject);
-        stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
-      });
-
-    // Get the object} from the Amazon S3 bucket. It is returned as a ReadableStream.
-    const data = await s3Client.send(new GetObjectCommand(financial_ds_params));
-      return data; // For unit tests.
-    // Convert the ReadableStream to a string.
-    const bodyContents = await streamToString(data.Body);
-    console.log(bodyContents);
-      return bodyContents;
-  } catch (err) {
-    console.log("Error", err);
-  }
-  try {
-    const data = await s3Client.send(new GetObjectCommand(financial_ds_params));
+    const data = await s3Client.send(new GetObjectCommand(bucketParams));
     const inputStream = data.Body;
-    const outputStream = createWriteStream("fds.csv");
     inputStream.pipe(outputStream);
     outputStream.on('finish', () => {
       console.log(`downloaded the file successfully`);
     });
-
   } catch (err) {
-    console.log( "Err", err)
+    console.log('Error', err);
   }
 }
 
