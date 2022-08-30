2deadb213e6502f439603abc8a1c402a8299d19e
VAKISHNA THAYALAN <19279795@students.ltu.edu.au>
2022-08-30 15:50:56 +1000

    Test Commit2

--- Report Pack/index.js
+++ Report Pack/index.js
@@ -1,13 +1,13 @@
 
-import { s3Client, s3params, uploadObject } from "./dataset_s3_fetch.mjs"
-
-
-
+var fetch_s3 = require("./dataset_s3_fetch.js")
 var Excel = require('exceljs');
+
+
+
 var workbook = new Excel.Workbook();
 workbook.xlsx.readFile('index.xlsx')//Change file name here or give file path
 .then(function() {
-    uploadObject();
+    fetch_s3.uploadObject();
     var worksheet = workbook.getWorksheet('Sheet1');
     var i=1;
     worksheet.eachRow({ includeEmpty: false }, function(row, rowNumber) {
